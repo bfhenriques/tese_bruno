eb52bde50d44fe411d2ed8c4a139ac3013a50994
bruno <bfhenriques178@gmail.com>
2020-12-12 23:29:24 +0000

    preview contents only in edit page

--- django/project/interface/static/interface/js/contents.js
+++ django/project/interface/static/interface/js/contents.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-
-    $('#file').on('change', function(){
+    /*$('#file').on('change', function(){
         var fullPath = document.getElementById('file').value;
-        console.log(fullPath);
-        console.log(fullPath.split('.').pop());
         var filename = fullPath.split('\\').pop();
         var fileExtension = fullPath.split('.').pop();
 
-        if (fileExtension == 'pdf') {
-            $('#pdf-preview').attr('href', filename);
-        } else {
+        var preview_row = document.getElementById('preview_row');
+        var preview_div = document.getElementById('preview_div');
+
+        if (fileExtension == 'jpeg' || fileExtension == 'png'){
             var reader = new FileReader();
                 reader.onload = function()
                 {
-                    var output = document.getElementById('output_image');
-                    output.src = reader.result;
+                    preview_row.removeAttribute('style', 'display')
+                    preview_div.innerHTML = '<img id="preview" class="img-preview" src="'+reader.result+'"/>'
                 }
                 reader.readAsDataURL(event.target.files[0]);
+        } else {
+
         }
-    });
+    });*/
 });
